3f0f425b601ced1480ef775500546ede5f44fc3d
NachoBasilio <ignacio.n.basilio.b@gmail.com>
2024-12-18 20:18:30 -0300

    Final clase

--- Clase-3/main.js
+++ Clase-3/main.js
@@ -1,58 +1,4 @@
-import styled from 'styled-components'
-import './App.css'
-import Text from './components/Text'
-import { Titulo } from './components/Titulo'
-
-
-
-const Button = styled.button`
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-
-  &:hover {
-    ${(props) => {
-      if (props.primary) {
-        return `
-          background-color: green;
-        `
-      }
-      if (props.secondary) {
-        return `
-          background-color: yellow;
-        `
-      }
-    }}
-  }
-
-  ${(props) => {
-    if (props.primary) {
-      return `
-        background-color: blue;
-      `
-    }
-    if (props.secondary) {
-      return `
-        background-color: red;
-      `
-    }
-  }}
-`
-//Dejo esto como ayuda memoria: cualquier componente que exista, con el uso de syled(Nombre del Componente), se le puede agregar estilos. Siempre mientras acepte className como prop.
-const TextPersonalizado = styled(Text)`
-  color: red;
-  font-size: 2rem;
-`
-
-
-
-function App() {
-  const productosLibreria = [
+const productosLibreria = [
     {
         id: 1,
         nombre: "Cuaderno A4",
@@ -125,36 +71,38 @@ function App() {
     }
 ];
 
-
-  return (
-    <>
-      <Titulo primary>Esto es un titulo</Titulo>
-      <TextPersonalizado>Hola mundo</TextPersonalizado>
-
-      {
-        productosLibreria.map(el => (
-          <h3 key={el.id}>{el.nombre}</h3>
-        ))
-      }
-      <Button
-        primary // Cambia a `primary` para probar el color azul de fondo
-        onClick={() => {
-          alert('Hola mundo')
-        }}
-      >
-        Enviar
-      </Button>
-
-      <Button 
-        secondary // Cambia a `secondary` para probar el color rojo de fondo
-        onClick={() => {
-          alert('Botón secundario presionado')
-        }}
-      >
-        Cancelar
-      </Button>
-    </>
-  )
+const forEachCoder = (arry, fun) => {
+    for (let i = 0; i < arry.length; i++) {
+        fun(arry[i])
+    }
 }
 
-export default App
+productosLibreria.forEach(el => {
+    console.log(el.nombre)
+})
+
+forEachCoder(productosLibreria, el => {
+    console.log(el.nombre)
+})
+
+
+const mapCoder = (arry, fun) => {
+    const newArray = []
+    for (let i = 0; i < arry.length; i++) {
+        newArray.push(fun(arry[i]))
+    }
+    return newArray
+}
+
+const arrayMapeadoConNuestroPropioMap = mapCoder(productosLibreria, (el)=>{
+    return el.nombre
+})
+
+console.log(arrayMapeadoConNuestroPropioMap)
+
+
+const arrayMapeadoConMap = productosLibreria.map((el)=>{
+    return el.nombre
+})
+
+console.log(arrayMapeadoConMap)
